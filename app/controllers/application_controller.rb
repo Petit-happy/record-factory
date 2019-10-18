e28859424dc1a55eb7254a0671c245a52cbb1f01
pantyori-na <kouzu.keinaka8@gmail.com>
2019-10-18 08:27:50 +0000

    sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+#sign in後に飛ぶページ
     def after_sign_in_path_for(resource)
         case resource
         when EndUser
@@ -7,8 +7,21 @@ class ApplicationController < ActionController::Base
         when AdminUser
           admin_root_path
         end
+    end
+#sign up前に飛ぶページ
+    def after_sign_up_path_for(resource)
+      case resource
+      when EndUser
+        session[:previous_url] || end_user_root_path
+      when AdminUser
+        session[:previous_url] || admin_root_path
       end
+#sign outした後に飛ぶページ
     def after_sign_out_path_for(resource)
-      end_user_root_path
+      case resource
+      when EndUser
+        end_user_root_path
+      when AdminUser
+        admin_root_path
     end
 end
