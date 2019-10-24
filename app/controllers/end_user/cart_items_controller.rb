df332e5b212c66b94b3390dd68a36581d8798d84
ogishimaisamu <isamn80579@gmail.com>
2019-10-24 11:27:22 +0000

    つかりた

--- app/controllers/end_user/cart_items_controller.rb
+++ app/controllers/end_user/cart_items_controller.rb
@@ -27,6 +27,11 @@ class EndUser::CartItemsController < ApplicationController
     # binding.pry
   end
 
+  # post_image = PostImage.find(params[:post_image_id])
+  # comment = current_user.post_comments.new(post_comment_params)
+  # comment.post_image_id = post_image.id
+  # comment.save
+  # redirect_to post_image_path(post_image)
 
   def destroy
     @cart_item.destroy
@@ -44,15 +49,28 @@ class EndUser::CartItemsController < ApplicationController
     end
   end
   def confirmation
+    @order = Order.new
+    @address = params[:address_id]
+    @delivery = Address.find(@address)
+    @method_of_payment = params[:method_of_payment]
+  
+    if @method_of_payment == 0
+        @payment = "クレジットカード"
+    elsif @method_of_payment == 1
+        @payment ="銀行振込"
+    else @mehod_of_payment == 2
+        @payment ="代引き"
+    end
   end
 
-  private
+private
     def set_cart_item
       @cart_item = CartItem.find(params[:id])
     end
     def cart_item_params
       params.require(:cart_item).permit(:end_user_id, :product_id, :cart_sum)
     end
-    
-    
+    def _params
+      params.permit(:name, :content)
+    end
 end
